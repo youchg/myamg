f79eb5db598a33979153edce10a863e8cdf1ffcc
youchg <youchg@lsec.cc.ac.cn>
2016-12-06 15:31:00 +0800

    test list.c

--- test/test_list.c
+++ test/test_list.c
@@ -3,26 +3,38 @@
 #include <time.h>
 #include "preprocess.h"
 #include "list.h"
+#include "tool.h"
 
 void print_dvec(double *dvec, int length);
 
 int main()
 {
     int i;
-    int length     = 10;
+    int length     = 1000000;
+    int max_val    = 100;
 
     //srand(1);
     srand((unsigned)time(NULL));
     double *dvec = (double*)calloc(length, sizeof(double));
-    for(i=0; i<length; i++) dvec[i] = (double)(rand()%length) + (double)rand()/RAND_MAX;
-    print_dvec(dvec, length);
+    double tb_rand = Get_time();
+    for(i=0; i<length; i++) dvec[i] = (double)(rand()%max_val) + (double)rand()/RAND_MAX;
+    //print_dvec(dvec, length);
+    double te_rand = Get_time();
+    printf("rand time: %f\n", te_rand - tb_rand);
 
-    DNode  *node       = (DNode *)malloc(length * sizeof(DNode));
-    DNode **local_head = (DNode**)malloc(length * 2 * sizeof(DNode*));
-    DNode **local_tail = (DNode**)malloc(length * 2 * sizeof(DNode*));
+    double tb_list = Get_time();
+    DNode  *node       = (DNode *)malloc(length  * sizeof(DNode));
+    DNode **local_head = (DNode**)malloc(max_val * sizeof(DNode*));
+    DNode **local_tail = (DNode**)malloc(max_val * sizeof(DNode*));
     DList list;
     DList_init(&list, node, local_head, local_tail, dvec, length);
-    DList_print(&list, local_head, local_tail);
+    //DList_print(&list, local_head, local_tail);
+    
+    int delete_index = rand() % length;
+    DList_delete(&list, node+delete_index, local_head, local_tail);
+    //DList_print(&list, local_head, local_tail);
+    double te_list = Get_time();
+    printf("list time: %f\n", te_list - tb_list);
 
     free(local_tail);
     free(local_head);
