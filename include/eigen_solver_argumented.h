7c8bdefcfc470128e6d526a216ffec91b952d994
youchg <youchg@lsec.cc.ac.cn>
2018-05-16 22:22:36 +0800

    Add argumented eigen solver

---
+++ include/eigen_solver_argumented.h
@@ -0,0 +1,10 @@
+#ifndef __AMG_EIGEN_SOLVER_H__
+#define __AMG_EIGEN_SOLVER_H__
+
+#include "multigrid.h"
+#include "amg_param.h"
+
+void Eigen_solver_amg_argumented(multigrid *amg, 
+                                 int nev, double *eval, double **evec, 
+                                 amg_param param);
+#endif
